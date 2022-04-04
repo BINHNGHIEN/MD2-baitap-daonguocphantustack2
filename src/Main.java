34ebdce6f2f8ef076667c719e5fa63919c4c78e6
BINHNGHIEN <haibinh.94.bui@gmail.com>
2022-04-04 13:34:32 +0700

    noidung

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+
+class Main
+{
+    public static void reverse(StringBuffer str)
+    {
+        int n = str.length();
+        Stack obj = new Stack(n);
+
+
+        int i;
+        for (i = 0; i < n; i++)
+            obj.push(str.charAt(i));
+
+        for (i = 0; i < n; i++)
+        {
+            char ch = obj.pop();
+            str.setCharAt(i,ch);
+        }
+    }
+
+    public static void main(String args[]) {
+        StringBuffer s = new StringBuffer("1234567");
+        reverse(s);
+        System.out.println("Reversed string is " + s);
+    }
+}
